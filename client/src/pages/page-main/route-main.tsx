93a30c3ffd8f1ea9ec0a3c1764b94cddb97d7080
Vasily Bobnev <ardxel@gmail.com>
2023-05-23 04:52:05 +0300

    Create route-main.tsx

---
+++ client/src/pages/page-main/route-main.tsx
@@ -0,0 +1,9 @@
+import MainPage from "./MainPage.tsx";
+import {RouteObject} from "react-router-dom";
+
+const main: RouteObject = {
+    path: '/',
+    element: <MainPage/>,
+}
+
+export default main;
